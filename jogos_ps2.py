903a69170618f933543aa57756a4b1d929c7b556
istalo <istalo1@hotmail.com>
2026-05-24 04:35:25 -0300

    ✏️ Melhora o titulo das funções, dando uma melhor clareza e evitando repetição de texto

--- jogos_ps2.py
+++ jogos_ps2.py
@@ -1,6 +1,7 @@
 import csv
 import datetime
 import plotly.express as px
+import webbrowser
 jogos = []
 
 with open("playstation2_games_merge.csv", mode="r", encoding="utf-8-sig") as arq:
@@ -14,7 +15,7 @@ def titulo(texto):
   print("-"*60)
 
 def informacoes_gerais(): 
-  titulo("Total de Jogos e Desenvolvedoras/Publicadoras Distintas")
+  titulo("Total de Jogos, Desenvolvedoras e Publicadoras Únicas")
   
   dev = {}
   pub = {}
@@ -27,9 +28,9 @@ def informacoes_gerais():
     
     pub[publicadora] = pub.get(publicadora, 0) + 1
 
-  print(f"Total de jogos na lista: {len(jogos)}")
-  print(f"Total de desenvolvedoras distintas: {len(dev)}")
-  print(f"Total de publicadoras distintas: {len(pub)}")
+  print(f"Jogos Lançados: {len(jogos)}")
+  print(f"Desenvolvedoras Únicas: {len(dev)}")
+  print(f"Publicadoras Únicas: {len(pub)}")
   print("-" * 60)
 
   resposta = input("Deseja gerar um gráfico com esses dados? (S/N): ").strip().upper()
@@ -40,7 +41,7 @@ def informacoes_gerais():
     fig = px.bar(
         x=['Jogos', 'Desenvolvedoras', 'Publicadoras'],
         y=valores,
-        title="Visão Geral da Base de Dados",
+        title="Visão Geral das Informações",
         text=valores
     )
 
@@ -49,7 +50,7 @@ def informacoes_gerais():
     fig.show()
 
 def primeiros_jogos():
-    titulo("Os Primeiros 20 Jogos Lançados")
+    titulo("Primeiros 20 Lançamentos")
 
     def arruma_data(data):
         ano = data[:4]
@@ -66,18 +67,18 @@ def primeiros_jogos():
     print("-"*60)
 
 def jogos_todas_regioes():
-  titulo("Jogos Lançados em Todas as Regiões")
+  titulo("Jogos Lançados Mundialmente")
 
   count = 0
   for jogo in jogos:
     if jogo['JP'] == 'Released' and jogo['EU/PAL'] == 'Released' and jogo['NA'] == 'Released':
       count += 1
 
-  print(f"Nº de Jogos Lançados em Todas as Regiões: {count}")
+  print(f"Total de Lançamentos Mundiais: {count}")
   print("-"*60)
 
 def jogos_regiao():
-  titulo("Jogos Exclusivos por Região")
+  titulo("Número de Exclusivos por Região")
 
   set_jp = set()
   set_eu = set()
@@ -94,9 +95,9 @@ def jogos_regiao():
   apenas_eu = set_eu - (set_jp | set_na)
   apenas_na = set_na - (set_jp | set_eu)
 
-  print(f"Nº de Jogos Exclusivos do Japão: {len(apenas_jp)}")
-  print(f"Nº de Jogos Exclusivos da Europa: {len(apenas_eu)}")
-  print(f"Nº de Jogos Exclusivos da América do Norte: {len(apenas_na)}")
+  print(f"Exclusivos do Japão: {len(apenas_jp)}")
+  print(f"Exclusivos da Europa: {len(apenas_eu)}")
+  print(f"Exclusivos da América do Norte: {len(apenas_na)}")
   print("-"*60)
 
   resposta = input("Deseja gerar um gráfico com esses dados? (S/N): ").strip().upper()
@@ -105,17 +106,17 @@ def jogos_regiao():
     fig = px.pie(
         names=['Japão', 'Europa', 'América do Norte'],
         values=[len(apenas_jp), len(apenas_eu), len(apenas_na)],
-        title="Distribuição de Jogos Exclusivos por Região"
+        title="Distribuição de Exclusivos por Região"
     )
     
     fig.show()
 
 while True:
   titulo("Análise de Dados: Jogos Lançados para PlayStation 2")
-  print("1. Total de Jogos e Desenvolvedoras/Publicadoras Distintas")
-  print("2. Os Primeiros 20 Jogos Lançados")
-  print("3. Jogos Lançados Em Todas as Regiões")
-  print("4. Jogos Exclusivos por Região")
+  print("1. Total de Jogos, Desenvolvedoras e Publicadoras Únicas")
+  print("2. Primeiros 20 Lançamentos")
+  print("3. Jogos Lançados Mundialmente")
+  print("4. Número de Exclusivos por Região")
   print("5. Finalizar")
   opcao = int(input("Opção: "))
   if opcao == 1:
@@ -126,5 +127,9 @@ while True:
     jogos_todas_regioes()
   elif opcao == 4:
     jogos_regiao()
-  else:
+  elif opcao == 5:
     break
+  elif opcao == 2000:
+    webbrowser.open("https://www.youtube.com/watch?v=Cxk1gI2sg3w")
+  else:
+    print("Opção inválida. Tente novamente...\n(Psst... Tem um easter egg nas opções!).")
