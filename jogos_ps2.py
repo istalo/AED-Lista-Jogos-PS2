538bc10927445b19ec98440afce0cabe95194805
istalo <istalo1@hotmail.com>
2026-05-24 03:27:01 -0300

    ✨ Adiciona as últimas 2 funções

--- jogos_ps2.py
+++ jogos_ps2.py
@@ -10,7 +10,7 @@ with open("playstation2_games_merge.csv", mode="r", encoding="utf-8-sig") as arq
 def titulo(texto):
   print()
   print(texto)
-  print("-"*40)
+  print("-"*60)
 
 def informacoes_gerais(): 
   titulo("Total de Jogos e Desenvolvedoras/Publicadoras Distintas")
@@ -29,7 +29,7 @@ def informacoes_gerais():
   print(f"Total de jogos na lista: {len(jogos)}")
   print(f"Total de desenvolvedoras distintas: {len(dev)}")
   print(f"Total de publicadoras distintas: {len(pub)}")
-  print("-" * 40)
+  print("-" * 60)
 
 def primeiros_jogos():
     titulo("Os Primeiros 20 Jogos Lançados")
@@ -46,13 +46,48 @@ def primeiros_jogos():
     for i in range(20):
         jogo = jogos_ordenados[i]
         print(f"{i+1}. {jogo['Title']} - Lançado em: {jogo['First released']}")
+    print("-"*60)
+
+def jogos_todas_regioes():
+  titulo("Jogos Lançados em Todas as Regiões")
+
+  count = 0
+  for jogo in jogos:
+    if jogo['JP'] == 'Released' and jogo['EU/PAL'] == 'Released' and jogo['NA'] == 'Released':
+      count += 1
+
+  print(f"Nº de Jogos Lançados em Todas as Regiões: {count}")
+  print("-"*60)
+
+def jogos_regiao():
+  titulo("Jogos Exclusivos por Região")
+
+  set_jp = set()
+  set_eu = set()
+  set_na = set()
+  for jogo in jogos:
+    if jogo['JP'] == 'Released':
+      set_jp.add(jogo['Title'])
+    if jogo['EU/PAL'] == 'Released':
+      set_eu.add(jogo['Title'])
+    if jogo['NA'] == 'Released':
+      set_na.add(jogo['Title'])
+
+  apenas_jp = set_jp - (set_eu | set_na)
+  apenas_eu = set_eu - (set_jp | set_na)
+  apenas_na = set_na - (set_jp | set_eu)
+
+  print(f"Nº de Jogos Exclusivos do Japão: {len(apenas_jp)}")
+  print(f"Nº de Jogos Exclusivos da Europa: {len(apenas_eu)}")
+  print(f"Nº de Jogos Exclusivos da América do Norte: {len(apenas_na)}")
+  print("-"*60)
 
 while True:
   titulo("Análise de Dados: Jogos Lançados para PlayStation 2")
   print("1. Total de Jogos e Desenvolvedoras/Publicadoras Distintas")
   print("2. Os Primeiros 20 Jogos Lançados")
-  print("3. Jogos Lançados na Europa porém não nos EUA")
-  print("4. Comparar Top 10 de um ano com Geral")
+  print("3. Jogos Lançados Em Todas as Regiões")
+  print("4. Jogos Exclusivos por Região")
   print("5. Finalizar")
   opcao = int(input("Opção: "))
   if opcao == 1:
@@ -60,8 +95,8 @@ while True:
   elif opcao == 2:
     primeiros_jogos()
   elif opcao == 3:
-    jogos_europa_nao_eua()
+    jogos_todas_regioes()
   elif opcao == 4:
-    informacoes_gerais()
+    jogos_regiao()
   else:
     break
